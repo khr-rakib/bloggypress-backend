bf8b24a4d1e33e730e7beaf9035bbf1e2afe3d55
khr-rakib <khr.rakib@gmail.com>
2021-04-15 13:10:53 +0600

    forgot & reset api done

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
 
-const { preRegister, register, login, logout, test } = require('../controllers/authController')
+const { preRegister, register, login, logout, test, forgotPassword, resetPassword } = require('../controllers/authController')
 const { runValidation } = require('../validators')
 const { userRegisterValidator } = require('../validators/authValidator')
 
 router.post('/pre-register', userRegisterValidator, runValidation, preRegister)
-router.post('/register', userRegisterValidator, runValidation, register)
+router.post('/register', register)
 router.post('/login', login)
 router.get('/logout', logout)
+router.put('/forgot-password', forgotPassword)
+router.put('/reset-password', resetPassword)
 
 
 // test route
